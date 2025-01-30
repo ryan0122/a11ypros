75a443475c98af2dfe75a6ef1580c0021d64b16a
ryan0122 <ryan.mack1200@gmail.com>
2025-01-29 20:33:39 -0700

    fix import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import Link from "next/link";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Nav from "@/components/nav";
+import Nav from "@/components/Nav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
