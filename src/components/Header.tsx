b687892bdb827b8f9975b4a7c314d51d176d4017
ryan0122 <ryan.mack1200@gmail.com>
2025-02-26 22:43:22 -0700

    more styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Header() {
 	return (
-		<div className="header flex flex-row items-center justify-evenly w-full bg-gray-800 py-3">
+		<div className="header flex flex-row items-center justify-evenly w-full bg-gray-800 py-3 sticky top-0 z-10">
 			<div className="logo">
 				<Link className="text-white" href="/">A11y <strong>Pros</strong></Link>
 			</div>
--- src/components/PageTemplate.tsx
+++ src/components/PageTemplate.tsx
@@ -6,8 +6,8 @@ interface PageProps {
 export default function PageTemplate({ title, content }: PageProps) {
   return (
     <>
-      <div className="page-container">
-        <h1 dangerouslySetInnerHTML={{ __html: title }} />
+      <div className="page-container max-w-7xl mx-auto p-8">
+        <h1 className="text-3xl font-semibold mb-6 text-center" dangerouslySetInnerHTML={{ __html: title }} />
         <div dangerouslySetInnerHTML={{ __html: content }} />
       </div>
     </>
