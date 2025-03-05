153deccc7499d098784f154c8d06a27259283077
ryan0122 <ryan.mack1200@gmail.com>
2025-03-04 22:07:57 -0700

    uppercase logo letters

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
 	return (
 		<div className="header flex flex-row items-center justify-evenly w-full py-3 sticky top-0 z-10">
 			<div className="logo">
-				<Link className="text-white hover:text-[#d4e300]" href="/">A11y <strong>Pros</strong></Link>
+				<Link className="text-white hover:text-[#d4e300]" href="/">A11Y <strong>PROS</strong></Link>
 			</div>
 			<TopNav/>
 			{/* Mobile navigation */}
