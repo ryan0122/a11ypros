8b0a803d850ae84ebb3edb8ef0c00d8ffe657a37
ryan0122 <ryan.mack1200@gmail.com>
2025-03-20 08:49:14 -0600

    styling for accorion

--- src/components/FaqAccordion.tsx
+++ src/components/FaqAccordion.tsx
@@ -12,14 +12,14 @@ type FAQProps = {
 
 const FAQAccordion: React.FC<FAQProps> = ({ faqs, title }) => {
   return (
-    <div className="max-w-4xl mx-auto mt-10 mb-20">
-      <h2 className="text-center text-3xl">{title}</h2>
+    <div className="faq-accordion max-w-4xl mx-auto mt-10 mb-20">
+      <h2 className="text-center text-3xl mb-5">{title}</h2>
       {faqs.map((faq, index) => (
-        <details key={index} className="border-b border-gray-300 p-4">
-          <summary className="text-lg font-semibold cursor-pointer list-none">
+        <details key={index} className="group border-b border-gray-300 p-4">
+          <summary className="text-lg font-semibold cursor-pointer flex items-center p-2 gap-3 before:content-['▶'] before:transition-transform before:duration-300 group-open:before:rotate-90">
             {faq.question}
           </summary>
-          <div className="mt-2 text-gray-700" dangerouslySetInnerHTML={{ __html: faq.answer }} />
+          <div className="mt-1 pl-2" dangerouslySetInnerHTML={{ __html: faq.answer }} />
         </details>
       ))}
     </div>
