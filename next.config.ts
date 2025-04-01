a6a985d4a2ba0b1a089777b604d9fa61bea274e9
ryan0122 <ryan.mack1200@gmail.com>
2025-04-01 09:14:39 -0600

    fix config for images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cms.a11ypros.com",
+        pathname: "/wp-content/uploads/**",
+      },
+    ],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
